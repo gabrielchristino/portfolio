01057143b4e34f6ad15fbd3c1d16d391b4373bc0
gabrielchristino <gabriel.christino@gmail.com>
2022-08-29 15:07:48 -0300

    feat: neumorfismo e ajustes gerais

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,23 +4,12 @@ function irPara(objetoNome) {
     principal.scrollTo({ top: objetoObj.offsetTop, behavior: 'smooth' });
 }
 
-var grid = document.querySelector(".grid");
-var emptyTile = document.querySelector(".tile--empty");
-var tiles = Array.from(document.querySelectorAll(".tile"));
-tiles.map(tile => {
-    tile.addEventListener("click", event => {
-      var tileArea = tile.style.getPropertyValue("--area");
-      var emptyTileArea = emptyTile.style.getPropertyValue("--area");
-  
-      emptyTile.style.setProperty("--area", tileArea);
-      tile.style.setProperty("--area", emptyTileArea);
+function moverQuadrado(quadrado) {
+    var quadrado0 = document.querySelector(".quadrado--0");
 
-      var atual = tile.className;
-      var vazio = emptyTile.className;
-      emptyTile.className = atual;
-      tile.className = vazio;
-      emptyTile = document.querySelector(".tile--empty");
-      tiles = Array.from(document.querySelectorAll(".tile"));
-    });
+    var atual = quadrado.className;
+    var vazio = quadrado0.className;
 
-  });
+    quadrado0.className = atual;
+    quadrado.className = vazio;
+}
